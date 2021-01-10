ed36a5d1f1e32c115d35bbb73f2ec41b147082f3
cvraindays@outlook.com <cvraindays@outlook.com>
2021-01-10 14:12:05 +0800

    Site updated: 2021-01-10 14:12:04

---
+++ js/pract.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+		    var config = {
+		        vx: 3,	
+		        vy: 3,	
+		        height: 4,	
+		        width: 4,
+		        count: 10,		
+		        color: "250, 243, 140", 	
+		        stroke: "239,239,239", 		
+		        dist: 10, 	
+		        e_dist: 20, 	
+		        max_conn: 15 	
+		    }
+
+		    CanvasParticle(config);
+		}
