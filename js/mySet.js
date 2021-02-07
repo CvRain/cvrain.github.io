b8cc3bf1ee13b53be81af72f20e9575eee6672cf
cvraindays@outlook.com <cvraindays@outlook.com>
2021-02-07 22:59:48 +0800

    Site updated: 2021-02-07 22:59:48

---
+++ js/mySet.js
@@ -0,0 +1,7 @@
+function showPassword() {
+    document.getElementById("showPwd").innerHTML = "&nbsp;|点我&nbsp;8482";
+}
+
+function leavePasswd(){
+    document.getElementById("showPwd").innerHTML = "";
+}
